3d4a4bf5c6c33d3ae77aff770450951fd074f530
acgsior <acgsior@outlook.com>
2018-06-01 01:29:33 +0800

    chore/feat: setup and swiper for index

---
+++ pages/index/index.js
@@ -0,0 +1,12 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    indexSwiperImgUrls: [
+      'https://data.whicdn.com/images/312830694/large.jpg',
+      'https://data.whicdn.com/images/312804411/large.png',
+      'https://data.whicdn.com/images/82096478/large.jpg'
+    ],
+  },
+})
