3ac89a7c66eb0d5e9f415faa3283dbe3ef113b62
acgsior <acgsior@outlook.com>
2018-06-03 00:53:57 +0800

    feat: menus render and load data from fungal server

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,22 @@ const app = getApp()
 Page({
   data: {
     indexSwiperImgUrls: [
-      'https://data.whicdn.com/images/312830694/large.jpg',
-      'https://data.whicdn.com/images/312804411/large.png',
-      'https://data.whicdn.com/images/82096478/large.jpg'
+      // 'https://data.whicdn.com/images/312830694/large.jpg',
+      // 'https://data.whicdn.com/images/312804411/large.png',
+      // 'https://data.whicdn.com/images/82096478/large.jpg'
     ],
+    indexMenus: [
+      // 'Home', 'Sells', 'Male', 'Female', 'Child'
+    ]
   },
+  onReady: function() {
+    wx.request({
+      url: 'http://127.0.0.1:8080/index',
+      success: ({ data }) => {
+        this.setData({
+          ...data
+        });
+      }
+    });
+  }
 })
