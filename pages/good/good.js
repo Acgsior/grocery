58034a21a20766e34b8007344cbfd2b41eab43c2
Morgan Qin <morgan.qin@activenetwork.com>
2018-06-04 17:37:44 +0800

    feat: good page and introduce base64 svg icon

---
+++ pages/good/good.js
@@ -0,0 +1,20 @@
+Page({
+  data: {
+    goodSwiperImgUrls: [
+      'https://data.whicdn.com/images/29671466/large.jpg',
+      'https://data.whicdn.com/images/88100882/large.jpg',
+      'https://data.whicdn.com/images/126023343/large.jpg',
+      'https://data.whicdn.com/images/313413008/large.jpg',
+    ],
+    good: {
+      name: 'Flat White',
+      priceBefore: 49,
+      price: 39,
+      promotion: {
+        type: 'SALE',
+        description: 'Up to 50% off for the icy summer.',
+      },
+    },
+    goodTips: ['Free shipping for $49'],
+  },
+});
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,52 +1,53 @@
 //index.js
-const app = getApp()
+const app = getApp();
 
 Page({
   data: {
     indexSwiperImgUrls: [
       'https://data.whicdn.com/images/312830694/large.jpg',
       'https://data.whicdn.com/images/312804411/large.png',
-      'https://data.whicdn.com/images/82096478/large.jpg'
+      'https://data.whicdn.com/images/82096478/large.jpg',
     ],
-    indexMenus: [
-      'Home', 'Sells', 'Male', 'Female', 'Child'
+    indexMenus: ['Home', 'SALE', 'Male', 'Female', 'Child'],
+    indexTips: ['Evaluation', 'Free Shipping for $49', 'Return Allowance'],
+    indexGoods: [
+      {
+        name: 'Flat White',
+        price: 39,
+        priceBefore: 49,
+        img: 'https://data.whicdn.com/images/29671466/large.jpg',
+      },
+      {
+        name: 'Cat',
+        price: 89,
+        priceBefore: 78,
+        img: 'https://data.whicdn.com/images/313355192/large.jpg',
+      },
+      {
+        name: 'Doggy',
+        price: 5570000,
+        priceBefore: 500000,
+        img: 'https://data.whicdn.com/images/313325092/large.jpg',
+      },
+      {
+        name: 'Strawberry Piece',
+        price: 19,
+        priceBefore: 18.88,
+        img: 'https://data.whicdn.com/images/313296838/large.jpg',
+      },
+      {
+        name: 'Rose',
+        price: 26,
+        priceBefore: 23,
+        img: 'https://data.whicdn.com/images/313288450/large.jpg',
+      },
+      {
+        name: 'Strawberry Pancake',
+        price: 12,
+        priceBefore: 8.99,
+        img: 'https://data.whicdn.com/images/313283807/large.jpg',
+      },
     ],
-    indexTips: [
-      'Evaluation',
-      'Free Shipping for $49',
-      'Return Allowance'
-    ],
-    indexGoods: [{
-      name: 'Flat White',
-      price: 39,
-      priceBefore: 49,
-      img: 'https://data.whicdn.com/images/29671466/large.jpg'
-    }, {
-      name: 'Cat',
-      price: 89,
-      priceBefore: 78,
-      img: 'https://data.whicdn.com/images/313355192/large.jpg'
-    }, {
-      name: 'Doggy',
-      price: 5570000,
-      priceBefore: 500000,
-      img: 'https://data.whicdn.com/images/313325092/large.jpg'
-    }, {
-      name: 'Strawberry Piece',
-      price: 19,
-      priceBefore: 18.88,
-      img: 'https://data.whicdn.com/images/313296838/large.jpg'
-    }, {
-      name: 'Rose',
-      price: 26,
-      priceBefore: 23,
-      img: 'https://data.whicdn.com/images/313288450/large.jpg'
-    }, {
-      name: 'Strawberry Pancake',
-      price: 12,
-      priceBefore: 8.99,
-      img: 'https://data.whicdn.com/images/313283807/large.jpg'
-    }]
   },
   onReady: function() {
     // wx.request({
@@ -59,5 +60,5 @@ Page({
     //     });
     //   }
     // });
-  }
-})
+  },
+});
